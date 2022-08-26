f13442580e24a925849e9f0de79490006bf67b50
Shacham Ginat <shacham.ginat@pagaya.com>
2022-08-26 11:42:18 +0300

    Looks good

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Awesome List
-## Python
-### Libraries
-#### Tui
-rich - Create beautiful CLI's using Python
-### Frameworks
-#### Testing
-pytest - The most popular Python testing framework
+# Shacham's Awesome List
+This is my awesome list, for various things.
+This is more for me, but if you find this list useful, cheers.
+# Python
+## Libraries
+### Tui
+- rich - Create beautiful CLI's using Python
+## Frameworks
+### Testing
+- pytest - The most popular Python testing framework
--- parser.py
+++ parser.py
@@ -21,24 +21,31 @@ groups = {}
 
 for item in content_spec:
     g = groups
+    if not item["tags"]:
+        g = g.setdefault("Rest", {})
     for p in item["tags"]:
         g = g.setdefault(p, {})
     group_items = g.setdefault("_items", [])
     group_items.append(item)
 
 
-md_content = []
+md_content = [
+    "# Shacham's Awesome List",
+    "This is my awesome list, for various things.",
+    "This is more for me, but if you find this list useful, cheers.",
+]
 
 
 def walk(group_name, group: dict, level=1):
     md_content.append(f"{'#' * level} {group_name}")
     for item in group.pop("_items", []):
-        md_content.append(f"{item['name']} - {item['description']}")
+        md_content.append(f"- {item['name']} - {item['description']}")
 
     for inner_group_name, inner_group in group.items():
         walk(inner_group_name, inner_group, level + 1)
 
 
-walk("Awesome List", groups)
+for group_name, group in groups.items():
+    walk(group_name, group)
 
 pathlib.Path("README.md").write_text("\n".join(md_content), encoding="utf-8")
